ccbebf8577a14030e7626b9e40b459d5949250f8
Alexandre Hitchcox <alexandre@hitchcox.me>
2020-05-11 21:22:00 +0100

    hmmm

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -60,7 +60,7 @@ function Home() {
 	const context = useDocusaurusContext();
 	const { siteConfig = {} } = context;
 	return (
-		<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+		<Layout description="Description will go into a meta tag in <head />">
 			<header className={classnames("hero hero--primary", styles.heroBanner)}>
 				<div className="container">
 					<h1 className="hero__title">{siteConfig.title}</h1>

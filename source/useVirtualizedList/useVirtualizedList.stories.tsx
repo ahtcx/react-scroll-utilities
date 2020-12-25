84a16a6bb209b94cae73d924c338ac0478d7b2d7
Alexandre Hitchcox <alexandre@hitchcox.me>
2020-12-25 16:05:12 +0100

    this is it, i think

--- source/useVirtualizedList/useVirtualizedList.stories.tsx
+++ source/useVirtualizedList/useVirtualizedList.stories.tsx
@@ -5,7 +5,7 @@ import { useVirtualizedList } from "./useVirtualizedList";
 export default { title: "useVirtualizedList" };
 
 export const VirtualizedList: React.FC = () => {
-	const items = Array.from({ length: 1000 }, (_, index) => index);
+	const items = Array.from({ length: 10000 }, (_, index) => index);
 
 	const [{ ref, onScroll }, virtualItems, { getItemOffset, getItemSize }] = useVirtualizedList(items, { overscan: 0 });
 
